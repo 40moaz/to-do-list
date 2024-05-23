c1a17b88b1d545a0b9a1370f07349e1442046490
40moaz <amoaz14109@gmail.com>
2024-05-23 23:04:55 +0200

    first commit

--- src/components/AccountSection/AccountData.tsx
+++ src/components/AccountSection/AccountData.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import avatar1 from "../../assets/avatar-1.jpg";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { menusActions } from "../../store/Menu.store";
-import LayoutMenus from "../Utilities/LayoutMenus";
+
 import DarkMode from "./DarkMode";
 import DeleteTasks from "./DeleteTasks";
+import LayoutMenus from "../Utilities/LayoutMenus";
+import React from "react";
 import TasksDone from "./TasksDone";
+import avatar1 from "../../assets/avatar-1.jpg";
+import { menusActions } from "../../store/Menu.store";
 
 const AccountData: React.FC = () => {
   const menuOpen = useAppSelector((state) => state.menu.menuAccountOpened);
@@ -25,7 +26,6 @@ const AccountData: React.FC = () => {
       <section className="p-5 flex flex-col h-full">
         <span className="flex items-center mx-auto">
           <span className="font-medium">Hi, User!</span>
-          <img src={avatar1} alt="cat" className="w-10 rounded-full ml-4" />
         </span>
 
         <DarkMode />
@@ -33,10 +33,10 @@ const AccountData: React.FC = () => {
         <TasksDone />
         <DeleteTasks />
         <a
-          href="https://github.com/x1-il"
+          href="https://www.linkedin.com/in/moaz-ali-9764662a3/"
           className="mt-4 bg-rose-100 p-2 rounded-md text-rose-600 text-center transition hover:bg-rose-200 dark:bg-slate-700/[.3] dark:text-slate-200"
         >
-          Projected by Ibrahim Laklaa
+          made by Moaz Ali
         </a>
       </section>
     </LayoutMenus>
